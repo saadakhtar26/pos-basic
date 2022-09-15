e0966f1b7a7f550c3afdf2d7fc6c6d7bcb17a8d2
Saad Rajpoot <saad.akhtar.2000@gmail.com>
2022-09-15 07:48:48 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,49 +1,38 @@
+<?php
+include("includes/db-connection.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="" type="http://localhost/pos/assets/images/icon/new.png">
-    <title>Bahria Town Company</title>
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
-        }
-        main{
-            margin-top: 200px;
-            text-align: center;
-        }
-        main a{
-            text-decoration: none;
-            color: #fff;
-            background-color: skyblue;
-            font-size: xxx-large;
-            font-weight: bolder;
-            margin: 25px;
-            padding: 80px;
-            border-radius: 50px;
-            border: 1px solid #999;
-        }
-        #first{
-            background-color: skyblue;
-        }
-        #first:hover{
-            background-color: blue;
-        }
-        #second{
-            background-color: lightgreen;
-        }
-        #second:hover{
-            background-color: green;
-        }
-    </style>
-</head>
+
+    <?php include("includes/head.php") ?>
+
 <body>
-    <main>
-        <a href="ply-wood/index.php" id="first">Ply Wood</a>
-        <a href="hardware/index.php" id="second">Hardware</a>
-    </main>
+    <div>
+        <?php include("includes/sidebar.php"); ?>
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <?php
+                        include("widgets.php");
+                        include("summary.php");
+                        include("includes/footer.php");
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+    </div>
+
+    <?php
+    include("includes/scripts.php");
+    mysqli_close($conn);
+    ?>
+
 </body>
 </html>
+<!-- end document-->
